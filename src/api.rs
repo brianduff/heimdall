d78676088ad60a64711b64662c3cfa673764ea3f
Brian Duff <cairndubh@gmail.com>
2021-03-17 16:58:44 -0700

    Render hostname in status bar

--- src/api.rs
+++ src/api.rs
@@ -1,11 +1,26 @@
+use std::ffi::OsString;
+
 use anyhow::Result;
 use rocket::Route;
+use rocket_contrib::json::Json;
+use serde::{Deserialize, Serialize};
 
 use crate::config;
 
+#[derive(Serialize, Deserialize, Debug)]
 struct Status {
-  is_configured: bool,
-  hostname: String,
+    is_configured: bool,
+    hostname: String,
+}
+
+#[get("/status")]
+fn status() -> Result<Json<Status>> {
+    let config = config::load()?;
+
+    Ok(Json(Status {
+        is_configured: !config.is_new(),
+        hostname: hostname::get()?.to_str().unwrap().to_owned(),
+    }))
 }
 
 #[get("/test")]
@@ -14,10 +29,11 @@ fn index() -> Result<String> {
 
     Ok(match config.is_new() {
         true => "Welcome!",
-        false => "Hello again!"
-    }.to_string())
+        false => "Hello again!",
+    }
+    .to_string())
 }
 
 pub fn get_routes() -> Vec<Route> {
-  routes![index]
+    routes![index, status]
 }
--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,41 @@
 
-Vue.component('status-bar', {
-  template: `
-  <div class="StatusBar">I am the status bar<span>
-  `
-})
+// Vue.component('status-bar', {
+//   template: `
+//   <div class="StatusBar">I am the status bar<span>
+//   `
+// })
 
-var app = new Vue({
-  el: "#app",
-  template: `
-    <status-bar>
-    Hello!
-  `
-})
+const Main = {
+  data() {
+    return {
+      status: {
+        hostname: "unknown"
+      },
+      loading: true
+    }
+  },
+  mounted() {
+    axios.get('/api/status').then(response => {
+      console.log("Got response", response)
+      this.loading = false
+      this.status = response.data
+    })
+  }
+}
+
+const app = Vue.createApp(Main)
+
+app.component('status-bar', {
+  props: ['status'],
+  template: `<div class="StatusBar">Host: {{status.hostname}}</div>`
+});
+
+app.mount('#app')
+
+// var app = new Vue({
+//   el: "#app",
+//   template: `
+//     <status-bar>
+//     Hello!
+//   `
+// })
